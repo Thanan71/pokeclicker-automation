f4401b4706966893e87211a6b483874a640a93af
Nathan PICOT <nathan.picot43@gmail.com>
2026-03-23 19:43:02 +0100

    correction

--- src/lib/Farm.js
+++ src/lib/Farm.js
@@ -32,6 +32,7 @@ class AutomationFarm {
             Automation.Utils.LocalStorage.setDefaultValue(this.Settings.UseRichMulch, false);
             Automation.Utils.LocalStorage.setDefaultValue(this.Settings.UseShovel, false);
             Automation.Utils.LocalStorage.setDefaultValue(this.Settings.SelectedBerryToPlant, BerryType.Cheri);
+            Automation.Utils.LocalStorage.setDefaultValue(this.Settings.ColburNonsenseEnabled, false);
 
             this.__internal__buildMenu();
         }
